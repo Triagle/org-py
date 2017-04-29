81045436b6fc8547a9be28799e506a7c53d5d326
Jake Faulkner <jakefaulkn@gmail.com>
2017-04-29 17:48:19 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+""" A library for parsing org mode files. """
+from setuptools import setup, find_packages
+
+setup (
+    name = 'py-org',
+    version = '1.0',
+    description = 'An org mode parser',
+    url = '',
+    author = 'Jake Faulkner',
+    author_email = 'jakefaulkn@gmail.com',
+    license = 'MIT',
+    keywords = 'org-mode',
+    packages = find_packages()
+)
